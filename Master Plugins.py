8e0402d84dbed52964b2fbbef8b56e3821f5b843
MattyG1850 <matthewgale1850@gmail.com>
2023-03-22 14:05:22 -0400

    Package Installation Guide added

--- Master Plugins.py
+++
@@ -1,4 +0,0 @@
-"""
-This is the master plugins/ modules file
-Run this first to ensure all necessary packages are installed to run the project
-"""
---
+++ Package Installation Guide.py
@@ -0,0 +1,29 @@
+"""
+This is the master plugins/ modules file
+Copy/Paste these lines separately into the terminal and run
+"""
+"""
+
+
+py -m ensurepip --upgrade   (This installs pip to be able to install other packages)
+
+pip install numpy
+
+pip install pandas
+
+python -m pip install xarray
+
+python -m pip install scipy
+
+python -m pip install -U matplotlib
+
+python -m pip install Pint
+
+python -m pip install metpy
+
+pip install cartopy
+
+pip install siphon
+
+pip install netCDF4
+"""
